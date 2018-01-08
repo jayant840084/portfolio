07c5b6e625e73b623b53966d02540d9f0e76d516
Jayant Singh <jayant840084@gmail.com>
2018-01-08 22:21:25 +0530

    Update

--- jayantsingh/jayantsingh/wsgi.py
+++ jayantsingh/jayantsingh/wsgi.py
@@ -8,6 +8,15 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# add the hellodjango project path into the sys.path
+sys.path.append(BASE_DIR)
+
+# add the virtualenv site-packages path to the sys.path
+sys.path.append(os.path.join(BASE_DIR, '../venv/lib64/python3.5/site-packages'))
 
 from django.core.wsgi import get_wsgi_application
 
